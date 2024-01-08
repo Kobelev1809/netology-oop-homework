9ab65877ab8d12cf7221d76c8449d532652cbccd
Igor Kobelev <kia1809@yandex.ru>
2024-01-08 22:31:33 +0300

    Task No. 2 is completed

--- classes.py
+++ classes.py
@@ -11,6 +11,14 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
+    def rate_lecture(self, lecture, course, grade):
+        if (isinstance(lecture, Lecturer)
+                and course in self.courses_in_progress
+                and course in lecture.courses_attached):
+            lecture.grades.setdefault(course, []).append(grade)
+        else:
+            return 'Ошибка: неверные данные при оценке лектора'
+
 
 class Mentor:
 
@@ -19,28 +27,23 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-    def rate_hw(self, student, course, grade):
-        if (isinstance(student, Student)
-                and course in self.courses_attached
-                and course in student.courses_in_progress):
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
-
 
 class Lecturer(Mentor):
 
     def __init__(self, name, surname):
         super().__init__(name, surname)
+        self.grades = {}
 
 
 class Reviewer(Mentor):
 
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
+    def rate_hw(self, student, course, grade):
+        if (isinstance(student, Student)
+                and course in self.courses_attached
+                and course in student.courses_in_progress):
+            student.grades.setdefault(course, []).append(grade)
+        else:
+            return 'Ошибка: неверные данные при оценке студента'
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
@@ -51,14 +54,23 @@ best_student.grades['Python'] = [10, 10]
 
 print(best_student.finished_courses)
 print(best_student.courses_in_progress)
-print(best_student.grades)
+print(best_student.grades, '\n')
 
-cool_mentor = Mentor('Some', 'Buddy')
+best_lecture = Lecturer('Some_Lec', 'Some_per')
+best_lecture.courses_attached.append('Python')
+
+cool_mentor = Reviewer('Some_Rev', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 print(cool_mentor.courses_attached)
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
 print(best_student.grades)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+print(best_student.grades)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+print(best_student.grades, '\n')
+
+print(best_lecture.grades)
+best_student.rate_lecture(best_lecture, 'Python', 9)
+print(best_lecture.grades)
+
